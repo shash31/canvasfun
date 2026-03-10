93cf10e83403f701acd0e7e483cbbc81d85d7d7a
shash123-d <88714649+shash123-d@users.noreply.github.com>
2026-03-10 04:17:32 -0700

    Added ugly clouds

--- rain.js
+++ rain.js
@@ -5,7 +5,7 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const gravity = 0.03
+const gravity = 0.04
 const wind = 0.5
 
 const maxHeight = 15
@@ -14,7 +14,13 @@ const minHeight = 5
 const maxWidth = 6
 const minWidth = 3
 
-const maxStartSpeed = 15
+const maxStartSpeed = 13
+
+const maxCloudWidth = 230
+const minCloudWidth = 150
+
+const maxCloudHeight = 100
+const minCloudHeight = 35
 
 const umbrellaHandleRadius = 30
 const umbrellaHeight = 130
@@ -26,7 +32,11 @@ c.strokeStyle = 'black' // Umbrella
 
 const mouse = {x: null, y: null};
 
-window.addEventListener('mousemove', (e) => {mouse.x = e.clientX; mouse.y = e.clientY; console.log(mouse.x, mouse.y)})
+window.addEventListener('mousemove', (e) => {mouse.x = e.clientX; mouse.y = e.clientY})
+
+function minmaxgenerator(max, min) {
+    return (Math.random()*(max-min))+min
+}
 
 class Drops {
     constructor() {
@@ -38,8 +48,8 @@ class Drops {
         this.y = 0;
         this.dx = Math.random()*wind * (Math.random() < 0.5 ? -1 : 1)
         this.dy = Math.random()*maxStartSpeed;
-        this.width = (Math.random()*(maxWidth-minWidth)) + minWidth
-        this.height = (Math.random()*(maxHeight-minHeight)) + minHeight
+        this.width = minmaxgenerator(maxWidth, minWidth)
+        this.height = minmaxgenerator(maxHeight, minHeight)
     }
 
     drop() {
@@ -53,7 +63,7 @@ class Drops {
                     this.dx = 1.5
                 }
 
-                this.dy = 0.4
+                this.dy = 0.8
             }
 
             this.x += this.dx
@@ -68,14 +78,66 @@ class Drops {
     }
 }
 
+class Clouds {
+    constructor() {
+        this.width = minmaxgenerator(maxCloudWidth, minCloudWidth)
+        this.x = Math.random()*(canvas.width-this.width)
+        this.y = Math.random()*(canvas.height*0.4) + (canvas.height*0.1)
+        this.dx = Math.random()*wind * (Math.random() < 0.5 ? -1 : 1)
+        this.opacity = Math.random()
+
+        this.points = []
+        const npoints = Math.floor(Math.random()*5)+1
+        for (let i = 0; i < npoints; i++) {
+            this.points.push(minmaxgenerator(maxCloudHeight, minCloudHeight))
+        }
+        this.points.push(0)
+
+        console.log(this)
+    }
+
+    draw() {
+        if (this.x < 0 || this.x+this.width > canvas.width) {
+            this.dx *= -1;
+        }
+        this.x += this.dx
+
+        c.fillStyle = `rgba(199, 196, 191, ${this.opacity})`
+        c.beginPath()
+        c.moveTo(this.x, this.y)
+        c.lineTo(this.x+this.width, this.y)
+        c.stroke()
+
+        for (let i = 0; i < this.points.length; i++) {
+            const x = this.width*(this.points.length - i - 1) / this.points.length
+            const cx = this.x+x+(x/2)
+            c.quadraticCurveTo(cx, this.y - maxCloudHeight, this.x+x, this.y - this.points[i])
+            c.stroke()
+        }
+
+        c.fill();
+    }
+}
+
 const rain = []
 
-for (let i = 0; i < 150; i++) {
+for (let i = 0; i < 200; i++) {
     rain.push(new Drops())
+}
+
+const clouds = []
+
+for (let i = 0; i < 8; i++) {
+    clouds.push(new Clouds())
 }
 
 function startRain() {
     c.clearRect(0, 0, canvas.width, canvas.height)
+
+    for (let i = 0; i < clouds.length; i++) {
+        clouds[i].draw()
+    }
+    
 
     for (let i = 0; i < rain.length; i++) {
         rain[i].drop()
